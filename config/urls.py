2fd89b85527fb88aa93a3ebe90bf4d53eb1259b0
Dmitry <hodas.work@gmail.com>
2023-10-19 20:15:10 +0300

    git hook add

--- config/urls.py
+++ config/urls.py
@@ -13,19 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url="mainapp/")),
-    path('mainapp/', include('mainapp.urls'))
-    
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,5 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', views.MainPageView.as_view()),
+    path("", views.MainPageView.as_view()),
 ]
