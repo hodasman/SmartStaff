85d0807ef2a937f232e64bfbae28f2569da34c5d
Dmitry <hodas.work@gmail.com>
2023-11-12 19:25:58 +0300

    Сделал шаблон для страницы списка устройств и добавил вьюху и урл

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
+
+
+class DevicesListView(TemplateView):
+    template_name = "mainapp/devices_list.html"
