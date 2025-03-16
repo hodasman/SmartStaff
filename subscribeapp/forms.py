8510a72036a44ba96be7a2dac804dc11fcde6725
Dmitry <hodas.work@gmail.com>
2025-03-16 13:04:52 +0300

    Из формы на подписку убрал label Email* который формировался автоматически

--- subscribeapp/forms.py
+++ subscribeapp/forms.py
@@ -5,6 +5,10 @@ from .models import Subscribers
 
 class SubscribeForm(forms.ModelForm):
     '''Форма подписки по email'''
+    def __init__(self, *args, **kwargs):
+        super(SubscribeForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = ""
+        
     def clean_name(self):
         name = self.cleaned_data.get('email')
 
